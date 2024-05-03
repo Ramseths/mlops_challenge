dce1c4ea07158b925340eef8667553b85a5c92ce
Jesus Ramseths <jesus.ramseths.contractor@pepsico.com>
2024-05-03 17:17:51 -0600

    feat: model inference

---
+++ api/app.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from joblib import load
+import pandas as pd
+
+model = load("../models/trained_model-1.0.0.joblib")
+
+class InputData(BaseModel):
+    # Features
+    floor_area: float
+    interest: float
+    cpi: float
+
+app = FastAPI()
+
+@app.post("/predict")
+def predict(data: InputData):
+    try:
+        # Model inference
+        input_data = pd.DataFrame({'Floor Area' : [data.floor_area],
+                                'Interest' : [data.interest],
+                                'CPI': [data.cpi]})
+        print(input_data)
+        prediction = model.predict(input_data)
+
+        return {"prediction": prediction.tolist()}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
